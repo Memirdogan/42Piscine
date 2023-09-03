ceaddea5a6226dc7173b00f96a4dfefbf31a7f68
Emir <66549233+MusaEmirDogan@users.noreply.github.com>
2023-09-03 18:42:05 +0300

    Rush00 %100

---
+++ Rush-00-100/rush00.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush00.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ddogan <ddogan@student.42istanbul.com.tr>  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/03 13:33:51 by ddogan            #+#    #+#             */
+/*   Updated: 2023/09/03 14:13:34 by ddogan           ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+void	ft_putchar(char c); // prototip
+
+void	satir_olustur(int line, char a_1, char b_2, char c_3)
+{
+	int	den;
+
+	den = 1;
+	while (den <= line)
+	{
+		if (den == 1)
+		{
+			ft_putchar(a_1);
+		}
+		else if (den > 1 && den < line)
+		{
+			ft_putchar(b_2);
+		}
+		else
+			ft_putchar(c_3);
+		den++;
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	iter;
+
+	iter = 1;
+	if (x <= 0 || y <= 0)
+	{
+		return ;
+	}
+	while (iter <= y)
+	{
+		if (iter == 1 || iter == y)
+			satir_olustur(x, 'o', '-', 'o');
+		else if (iter > 1 && iter < y)
+			satir_olustur(x, '|', ' ', '|');
+		iter++;
+	}
+}
