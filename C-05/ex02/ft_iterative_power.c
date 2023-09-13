4383601104689ec687dcd237eb514a9c2d92f2ea
Emir <66549233+MusaEmirDogan@users.noreply.github.com>
2023-09-13 08:37:03 +0300

    prototype

---
+++ C-05/ex02/ft_iterative_power.c
@@ -0,0 +1,20 @@
+int	ft_iterative_power(int nb, int power)
+{
+	int	holder;
+
+	holder = nb;
+	while (power > 1) 
+	{
+		holder *= nb; 
+		power--; 
+	}
+    if (power == 0)
+        return (1);
+    if (power < 0)
+        return (0);
+    return (holder);
+}
+/*int	main(void)
+{
+	printf("%i", ft_iterative_power(3, 4));
+}*/
