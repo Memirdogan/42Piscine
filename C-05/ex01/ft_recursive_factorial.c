e5895d992f2f0ce2daa0840ed5ae3d211408f0c1
Emir <66549233+MusaEmirDogan@users.noreply.github.com>
2023-09-16 14:47:19 +0300

    Delete C-05 directory

--- C-05/ex01/ft_recursive_factorial.c
+++
@@ -1,9 +0,0 @@
-int	ft_recursive_factorial(int nb)
-{
-	if (nb >= 2)// 0!=1 && 1!=1 o yüzden else
-		return (nb * ft_recursive_factorial(nb - 1));
-	else if (nb < 0)
-		return (0);
-	else
-		return (1);
-}
