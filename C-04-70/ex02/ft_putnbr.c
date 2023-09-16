9ab1e14cc2954b61c7d1f7d33fc4dedad87fb0a3
Emir <66549233+MusaEmirDogan@users.noreply.github.com>
2023-09-16 19:17:48 +0300

    C04 %70

---
+++ C-04-70/ex02/ft_putnbr.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mudogan <mudogan@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/16 13:29:26 by mudogan           #+#    #+#             */
+/*   Updated: 2023/09/16 13:29:32 by mudogan          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putchar('1');
+		write(1, "47483648", 8);
+	}
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putnbr(-nb);
+	}
+	else
+	{
+		if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+		}
+		ft_putchar('0' + nb % 10);
+	}
+}
