976ee77bf44bf45ba190399efc52828494f50a13
Emir <66549233+MusaEmirDogan@users.noreply.github.com>
2023-09-18 17:47:28 +0300

    C06 %100

---
+++ C-06-75/ex03/ft_sort_params.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yguclu <yguclu@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/15 21:58:40 by yguclu            #+#    #+#             */
+/*   Updated: 2023/09/18 02:13:39 by yguclu           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s2[i])
+	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
+	}
+	return (s1[i] - s2[i]);
+}
+
+void	swap1(char **str1_ptr, char **str2_ptr)
+{
+	char	*temp;
+
+	temp = *str1_ptr;
+	*str1_ptr = *str2_ptr;
+	*str2_ptr = temp;
+}
+
+void	sort_params(int count, char **arr)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	while (i < count - 1)
+	{
+		j = 1;
+		while (j < count - 1)
+		{
+			if (ft_strcmp(arr[j], arr[j + 1]) > 0)
+				swap1(&arr[j], &arr[j + 1]);
+			j++;
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	j = 0;
+	i = 1;
+	sort_params(argc, argv);
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			write(1, &argv[i][j], 1);
+			j++;
+		}
+		write(1, "\n", 1);
+		j = 0;
+		i++;
+	}
+}
