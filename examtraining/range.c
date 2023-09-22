5077904a99980bc41cac9342bb1b744f0ec08722
Emir <66549233+MusaEmirDogan@users.noreply.github.com>
2023-09-22 21:12:28 +0300

    exam soruları örnek çözüm

---
+++ examtraining/range.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mudogan <mudogan@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/20 18:53:23 by mudogan           #+#    #+#             */
+/*   Updated: 2023/09/20 19:08:21 by mudogan          ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	*ft_range(int min, int max)
+{
+	int	*p;
+	int	i;
+
+	if (min >= max)
+		return (0);
+	p = malloc((max - min) * 4);
+	if (p == NULL)
+		return (0);
+	i = 0;
+	while (min < max)
+	{
+		p[i] = min;
+		i++;
+		min++;
+	}
+	return (p);
+}
